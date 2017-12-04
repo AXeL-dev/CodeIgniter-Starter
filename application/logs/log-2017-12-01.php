25a6c12bd150966e98a7f69e8375e5c0925d5556
AXeL <anass_denna@hotmail.fr>
2017-12-04 10:41:17 +0000

    remove log files

--- application/logs/log-2017-12-01.php
+++
@@ -1,9 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2017-12-01 15:51:09 --> 404 Page Not Found: /index
-ERROR - 2017-12-01 15:51:16 --> 404 Page Not Found: /index
-ERROR - 2017-12-01 15:52:17 --> 404 Page Not Found: /index
-ERROR - 2017-12-01 15:52:52 --> 404 Page Not Found: /index
-ERROR - 2017-12-01 15:54:07 --> 404 Page Not Found: /index
-ERROR - 2017-12-01 18:42:18 --> 404 Page Not Found: /index
-ERROR - 2017-12-01 18:44:47 --> 404 Page Not Found: /index
